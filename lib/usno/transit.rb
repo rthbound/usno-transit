4ce2cd707591274247a543c97827e3793acd01db
rthbound <tad.hosford@gmail.com>
2013-11-21 03:12:17 -0500

    Align on LT symbol

--- lib/usno/transit.rb
+++ lib/usno/transit.rb
@@ -36,37 +36,37 @@ module USNO
       end
     end
 
-    class Sun < View;end
-    class Moon < View;end
-    class Mercury < View;end
-    class Venus < View;end
-    class Mars < View;end
-    class Jupiter < View;end
-    class Saturn < View;end
-    class Uranus < View;end
-    class Neptune < View;end
-    class Pluto < View;end
-    class Achernar < View;end
-    class Adhara < View;end
-    class Aldebaran < View;end
-    class Altair < View;end
-    class Antares < View;end
-    class Arcturus < View;end
-    class Betelgeuse < View;end
-    class Canopus < View;end
-    class Capella < View;end
-    class Deneb < View;end
-    class Fomalhaut < View;end
-    class Hadar < View;end
-    class Mimosa < View;end
-    class Polaris < View;end
-    class Pollux < View;end
-    class Procyon < View;end
-    class Regulus < View;end
-    class Rigel < View;end
+    class Sun            < View;end
+    class Moon           < View;end
+    class Mercury        < View;end
+    class Venus          < View;end
+    class Mars           < View;end
+    class Jupiter        < View;end
+    class Saturn         < View;end
+    class Uranus         < View;end
+    class Neptune        < View;end
+    class Pluto          < View;end
+    class Achernar       < View;end
+    class Adhara         < View;end
+    class Aldebaran      < View;end
+    class Altair         < View;end
+    class Antares        < View;end
+    class Arcturus       < View;end
+    class Betelgeuse     < View;end
+    class Canopus        < View;end
+    class Capella        < View;end
+    class Deneb          < View;end
+    class Fomalhaut      < View;end
+    class Hadar          < View;end
+    class Mimosa         < View;end
+    class Polaris        < View;end
+    class Pollux         < View;end
+    class Procyon        < View;end
+    class Regulus        < View;end
+    class Rigel          < View;end
     class RigilKentaurus < View;end
-    class Sirius < View;end
-    class Spica < View;end
-    class Vega < View;end
+    class Sirius         < View;end
+    class Spica          < View;end
+    class Vega           < View;end
   end
 end
